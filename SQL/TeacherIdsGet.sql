101326ce029e5f3df6b9fd21aa7b4928a3eb38f7
LucasAlonso11800 <lucasalonso11800@gmail.com>
2023-06-24 23:03:14 -0300

    Added and updated SP

---
+++ SQL/TeacherIdsGet.sql
@@ -0,0 +1,19 @@
+CREATE DEFINER=`root`@`%` PROCEDURE `TeacherIdsGet`(
+	OUT Rcode 		int,
+    OUT Rmessage 	varchar(100)
+)
+SP:BEGIN
+
+DECLARE EXIT HANDLER FOR SQLEXCEPTION
+BEGIN
+	GET DIAGNOSTICS CONDITION 1
+    Rmessage = MESSAGE_TEXT;
+    SET Rcode := 0;
+END;
+
+SET Rcode := 1;
+SET Rmessage := NULL;
+
+select 	teacher_id
+from	teachers;
+END
